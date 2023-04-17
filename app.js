43bfac83a337f8230c82cbdca298904a76df531b
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-04-17 08:02:44 -0400

    add controllers

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 // DEPENDENCIES
 const express = require("express");
-// const logsController = require("./controllers/logController.js");
-const cors = require('cors')
+const budgetController = require("./controllers/budgetController.js");
+const cors = require("cors");
 
 // CONFIGURATION
 const app = express();
 app.use(express.json()); // json middleware parses incoming requests with JSON payloads
-app.use(cors())
+app.use(cors());
 
 // ROUTES
 app.get("/", (req, res) => {
   res.send("Welcome to the Budget Tracking");
 });
 
-// app.use("/logs", logsController);
+// app.use("/budgets", budgetController);
 
 app.get("*", (req, res) => {
   res.status(404).json({ error: "Sorry, page not found!" });
