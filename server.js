05aba8eac915aacd7cf359583ebbb068326e42c1
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-04-18 11:53:57 -0400

    updated the PORT

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = require("./app.js");
 
 // CONFIGURATION
 require("dotenv").config();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT
 
 // LISTEN
 app.listen(PORT, () => {
