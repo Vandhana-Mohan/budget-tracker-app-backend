47d0f8bfe5d7ae78d6828912ef4571844bade0f1
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-04-18 23:46:49 -0400

    edit validation

--- models/validators.js
+++ models/validators.js
@@ -1,21 +1,21 @@
-const budgetValidator = (req, res, next) => {
-  const { id, item_name, amount, date, from, category } = req.body; //destructure properties from req.body object
-  if (id && item_name && amount && date) {
-    if (
-      typeof id === "string" &&
-      typeof item_name === "string" &&
-      typeof amount === "number" &&
-      typeof date === "string" &&
-      (!from || typeof from === "string") &&
-      (!category || typeof category === "string")
-    ) {
-      next();
-    } else {
-      res.status(400).json({ error: "Invalid transaction data" });
-    }
-  } else {
-    res.status(400).json({ error: "Transaction data is missing" });
-  }
-};
+// const budgetValidator = (req, res, next) => {
+//   const { id, item_name, amount, date, from, category } = req.body; //destructure properties from req.body object
+//   if (id && item_name && amount && date) {
+//     if (
+//       typeof id === "string" &&
+//       typeof item_name === "string" &&
+//       typeof amount === "number" &&
+//       typeof date === "string" &&
+//       (!from || typeof from === "string") &&
+//       (!category || typeof category === "string")
+//     ) {
+//       next();
+//     } else {
+//       res.status(400).json({ error: "Invalid transaction data" });
+//     }
+//   } else {
+//     res.status(400).json({ error: "Transaction data is missing" });
+//   }
+// };
 
-module.exports = budgetValidator;
+// module.exports = budgetValidator;
